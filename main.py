612498ad455e0fc947acb3768333e4c15d4eed9d
John Mercouris <jmercouris@gmail.com>
2016-09-16 00:50:06 -0500

    Calibration done in build method before returning main application window

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+import speech_recognition as sr
+
+r = sr.Recognizer()
+m = sr.Microphone()
 
 
 # Root Widget
@@ -10,8 +14,38 @@ class Root(Widget):
 class SpeechApp(App):
     
     def build(self):
+        print("A moment of silence, please...")
+        with m as source:
+            r.adjust_for_ambient_noise(source)
+            print("Set minimum energy threshold to {}".format(r.energy_threshold))
         return Root()
 
 
 if __name__ == '__main__':
     SpeechApp().run()
+
+
+    import speech_recognition as sr
+
+
+# try:
+    
+#     # Main loop
+#     while True:
+#         print("Say something!")
+#         with m as source:
+#             audio = r.listen(source)
+#         print("Got it! Now to recognize it...")
+        
+#         try:
+#             # recognize speech using Google Speech Recognition
+#             value = r.recognize_google(audio)
+#             print("You said {}".format(value))
+        
+#         except sr.UnknownValueError:
+#             print("Oops! Didn't catch that")
+        
+#         except sr.RequestError as e:
+#             print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+# except KeyboardInterrupt:
+#     pass
